8a0d263bcca84e705bd103c65130b6c26149fb41
Tushar <tushar.jadhav.work@gmail.com>
2024-05-03 12:12:25 +0530

    UC6 Add Multiple AddressBooks

---
+++ src/main/java/org/example/addressbook/AddressBook.java
@@ -0,0 +1,14 @@
+package org.example.addressbook;
+
+public class AddressBook {
+
+    public static void main(String[] args) {
+
+        Contact contact = new Contact();
+
+        System.out.println("***  Welcome To Address Book  ***");
+        contact.menuOption(contact);
+
+    }
+}
+
